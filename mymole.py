2acd98a6956ed7155d33be7f417c94ffe3f94983
manjunathroyal <manjunathroyal76@gmail.com>
2025-09-06 12:51:58 +0530

    Add files via upload

---
+++ mymole.py
@@ -0,0 +1,18 @@
+def add():
+    '''which used to add the valus'''
+    a=10
+    b=20
+    c=a+b
+    print(c)
+def sub():
+    '''which used to sub the valus'''
+    a=10
+    b=20
+    c=a-b
+    print(c)
+def mul():
+    '''which used to mul the valus'''
+    a=10
+    b=20
+    c=a*b
+    print(c)
